950ae9b8cf9173c10cdaf11453a94a703a31a801
limonzishad <zishadlimon@gmail.com>
2022-03-11 02:13:24 +0600

    Q&A section added

--- js/app.js
+++ js/app.js
@@ -134,7 +134,6 @@ const createPost = (post) => {
 const showPosts = (posts) => {
   const productsContainer = document.getElementById("posts");
   productsContainer.innerHTML = "";
-
   posts.forEach((post) => {
     const div = createPost(post);
     productsContainer.appendChild(div);
